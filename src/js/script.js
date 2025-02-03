3f4f75ab3658198dfd76d5eddebd08a24d72a625
Fredrick Appiah <apfred18@gmail.com>
2025-02-03 00:27:07 +0000

    landing page completed

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,18 @@ AOS.init({
   duration: 1000,
   offset: 100,
 });
+
+const mobileHeaderMenu = document.querySelector(".header__menu-mobile ul");
+const mobileHeaderMenuIcon = document.querySelector(".header__menu-mobile img");
+let menuExpanded = false;
+
+mobileHeaderMenuIcon.addEventListener("click", () => {
+  console.log("first");
+  menuExpanded = !menuExpanded;
+
+  if (menuExpanded) {
+    mobileHeaderMenu.style.display = "flex";
+  } else {
+    mobileHeaderMenu.style.display = "none";
+  }
+});
